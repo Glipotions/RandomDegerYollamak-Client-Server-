51b178d336a23d28ed42bb40c1c2e6d9e24b64aa
Kumru <Kumru@DESKTOP-QNRNBT0>
2021-08-16 14:38:05 +0300

    Proje dosyası ekle.

---
+++ Client/ClientProgram.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Random rastgele = new Random();
+            //int basamak=0;
+            connection:
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = new TcpClient("127.0.0.1", 1302);
+                    Console.WriteLine("*********BURASI CLİENT*********");
+
+                    //string messageToSend = "Clientten Servera Mesaj..";
+                    //string messageToSend = Convert.ToString(rastgele.Next());
+
+                    Console.WriteLine("Hangi işlemi yapmak istiyorsunuz?\n\"1\"-Random Sayı\n\"2\"-Random Harf");
+                    string messageToSend = Console.ReadLine();
+
+
+                    int byteCount = Encoding.ASCII.GetByteCount(messageToSend + 1);
+                    byte[] sendData = Encoding.ASCII.GetBytes(messageToSend);
+
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(sendData, 0, sendData.Length);
+                    Console.WriteLine("sending data to server...");
+
+                    StreamWriter sw = new StreamWriter(stream);
+                    sw.WriteLine(messageToSend);
+                    sw.Flush();
+                    StreamReader sr = new StreamReader(stream);
+                    string response = sr.ReadLine();
+                    Console.WriteLine(response);
+
+                    stream.Close();
+                    client.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("failed to connect...");
+                Console.WriteLine(e);
+                goto connection;
+            }
+        }
+    }
+}
---
+++ Server/ServerProgram.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string backResponse;
+            Random rastgele = new Random();
+
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1302);
+            listener.Start();
+
+
+            Console.WriteLine("*********BURASI SERVER*********");
+            while (true)
+            {
+
+                Console.WriteLine("Waiting for a connection.");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Client accepted.");
+                NetworkStream stream = client.GetStream();
+
+                StreamReader sr = new StreamReader(client.GetStream());
+                StreamWriter sw = new StreamWriter(client.GetStream());
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    stream.Read(buffer, 0, buffer.Length);
+                    int recv = 0;
+                    foreach (byte b in buffer)
+                    {
+                        if (b != 0)
+                        {
+                            recv++;
+                        }
+                    }
+
+                    string response = sr.ReadLine();
+                    if (response == "1")
+                    {
+                        backResponse = Convert.ToString(rastgele.Next());
+                    }else if (response == "2")
+                    {
+                        char karakter = Convert.ToChar(rastgele.Next(65, 91));
+                        //Console.WriteLine(karakter);
+                        backResponse = Convert.ToString(karakter);
+                    }
+                    else
+                    {
+                        backResponse = "Yanlış Seçim";
+                    }
+
+                    Console.WriteLine(response);
+                    Console.WriteLine("Serverda Üretilen Random Değer: " + backResponse);
+
+                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
+                    Console.WriteLine("request received");
+                    //sw.WriteLine(response);
+                    sw.WriteLine(backResponse);
+                    sw.Flush();
+                    
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Something went wrong.");
+                    sw.WriteLine(e.ToString());
+                }
+            }
+        }
+    }
+}
